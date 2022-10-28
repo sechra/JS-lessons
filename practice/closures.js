710055a57d81bca11c499fbb471638d860a9acf5
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2022-10-28 13:38:03 -0700

    updated code

--- practice/closures.js
+++ practice/closures.js
@@ -30,3 +30,34 @@ alert(name)
 
 var myFunc = makeFunc()
 myFunc()
+
+
+function makeAdder(x) {
+return function(y) {
+    return x + y
+}
+}
+
+var add5 = makeAdder(5)
+var add10 = makeAdder(10)
+
+console.log(add5(2))  //7
+console.log(add10(2)) // 12
+
+
+
+
+
+function makeSizer(size) {
+return function() {
+document.body.style.fontSize = size + 'px'
+}
+}
+
+
+var size12 = makeSizer(12)
+var size14 = makeSizer(14)
+var size16 = makeSizer(16)
+
+document.getElementBuId('size-12').onclick = size12
+
